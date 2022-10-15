07f5b297aa5aedb9cb65b96a0131e04032e55347
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-10-15 19:16:04 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - print a line of code using puts 
+/**
+ * main - print a line of code using puts
  *
  * Return: 0
  */
+
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
