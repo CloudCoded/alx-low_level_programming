73b3f2a52271a6cadf32c6f91aec042076f598ad
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-10-16 22:52:27 +0000

    base 16 num

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print num of base 16 in lowercase, followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char la;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+
+	for (la = 'a'; la <= 'f'; la++)
+		putchar(la);
+
+	putchar('\n');
+
+	return (0);
+}
