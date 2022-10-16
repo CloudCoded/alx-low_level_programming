2a3e4bdf058b8fc858728ce3d61662fc8b97a40c
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-10-16 15:59:14 +0000

    Dtypes sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ int main(void)
 	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
 	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
 	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(e));
+	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
 	return (0);
 }
