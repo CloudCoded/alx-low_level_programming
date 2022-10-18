ab3f148845213e11d99527c64ada7ba3002a87ea
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-10-18 16:24:18 +0000

    lc alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lc
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+
+	return (0);
+}
