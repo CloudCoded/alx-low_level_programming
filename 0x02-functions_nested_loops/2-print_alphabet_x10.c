5abdd7a793c34f5f86afd7d8197de0a3c9010828
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-10-18 17:09:37 +0000

    lc alphabet 10times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print 10 times the alphabet in lc
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
  */
 
 void print_alphabet_x10(void)
 {
-	int count = 0
+	int count = 0;
 	char lc;
 
 	while (count++ <= 9)
