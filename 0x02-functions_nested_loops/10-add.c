2324da466c87273fedab3b55774def6c8bf79612
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-10-19 05:52:22 +0000

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @q: the first number
+ * @w: the second number
+ * Return: the addition of both
+ */
+int add(int q, int w)
+{
+	return (q + w);
+}
