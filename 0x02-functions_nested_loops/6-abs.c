1615f1da4c768805a64499530aada0930261641f
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-10-19 02:59:59 +0000

    abs value of int

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - computes the abs value of an int
+ * @n: the int in question
+ *
+ * Return: the abs value of the int
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
