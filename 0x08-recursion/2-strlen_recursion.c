df6179cc9d56509449241e8160697f894219b02b
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-11-02 21:58:37 +0000

    print the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: the string to be measured
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1);
+	}
+
+	return (len);
+}
