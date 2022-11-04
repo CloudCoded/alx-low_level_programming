e6aac58067d1cc020260439e29669a00a72ccd91
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-11-04 18:30:10 +0000

    betty and checker respected

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,7 +7,7 @@
  * @argv: arguments
  * Return: 0
  */
-int main(int argc, char *argv[] _attribute_((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 
