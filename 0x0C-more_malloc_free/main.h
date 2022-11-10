8e8bfcfcedf8714f52c5aad9a0f5398bc7b963b4
CloudCoded <abdulazeezakindolapo@gmail.com>
2022-11-10 17:30:13 +0000

    more malloc free 100

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,6 +10,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int _putchar(char c);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 
 #endif
